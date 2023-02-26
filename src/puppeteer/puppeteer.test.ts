c721c43eac30420e957038b3b5a7a21402113139
robertoshimizu <rshimizubr@gmail.com>
2023-02-26 13:48:56 -0300

    feat: connect to exsiting page and perform a query

--- src/puppeteer/puppeteer.test.ts
+++ src/puppeteer/puppeteer.test.ts
@@ -1,26 +1,53 @@
 import { Browser, Page } from 'puppeteer'
 import puppeteer from 'puppeteer'
 
-describe('Puppeteer', () => {
+// describe('Puppeteer - launch a new page', () => {
+//   let browser: Browser
+//   let page: Page
+
+//   beforeAll(async () => {
+//     browser = await puppeteer.launch({
+//       headless: false
+//     })
+//     page = await browser.newPage()
+//   })
+
+//   afterAll(async () => {
+//     await browser.close()
+//   })
+
+//   test('should launch a browser, navigate to Google and search for "puppeteer"', async () => {
+//     await page.goto('https://www.google.com', {
+//       waitUntil: 'networkidle2',
+//       timeout: 12000
+//     })
+//     await page.type('input', 'puppeteer')
+//     await page.keyboard.press('Enter')
+//     await page.waitForNavigation()
+//     const title = await page.title()
+//     console.log(title)
+//     expect(title).toContain('puppeteer')
+//   })
+// })
+
+describe('Puppeteer - connect to a opened browser', () => {
   let browser: Browser
   let page: Page
 
   beforeAll(async () => {
-    browser = await puppeteer.launch({
-      headless: false
-    })
-    page = await browser.newPage()
+    const browserURL = 'http://127.0.0.1:9222'
+    browser = await puppeteer.connect({ browserURL })
+    console.log(browser.isConnected())
   })
 
   afterAll(async () => {
-    await browser.close()
+    browser.disconnect()
   })
 
-  it('should navigate to Google and search for "puppeteer"', async () => {
-    await page.goto('https://www.google.com', {
-      waitUntil: 'networkidle2',
-      timeout: 12000
-    })
+  test('should launch a browser, navigate to Google and search for "puppeteer"', async () => {
+    const page = (await browser.pages())[0]
+    console.log(page.url())
+    await page.goto('https://www.google.com')
     await page.type('input', 'puppeteer')
     await page.keyboard.press('Enter')
     await page.waitForNavigation()
