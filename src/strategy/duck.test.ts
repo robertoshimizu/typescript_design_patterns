de3e3fc0a32eda69f5d6c143d055e034f3977930
Roberto Shimizu <rshimizubr@gmail.com>
2023-02-26 11:22:04 -0300

    test: add mallard duck basic test

--- src/strategy/duck.test.ts
+++ src/strategy/duck.test.ts
@@ -1,9 +1,42 @@
-function sum(a: number, b: number): number {
-  return a + b
+abstract class Duck {
+  constructor() {}
+  quack: (() => void) | undefined
+  swim!: () => void
+  display!: () => void
 }
 
-describe('sum', () => {
-  it('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3)
+class MallardDuck extends Duck {
+  constructor() {
+    super()
+  }
+  quack = () => console.log('Quack')
+  swim = () => console.log('Swim')
+  display = () => console.log('I am a Mallard Duck')
+}
+
+describe('Duck behaviors', () => {
+  test('Mallard duck should quack', () => {
+    const duck = new MallardDuck()
+    const spy = jest.spyOn(duck, 'quack')
+    duck.quack()
+    expect(duck.quack).toBeCalled()
+    spy.mockReset()
+    spy.mockRestore()
+  })
+  test('Mallard duck should swim', () => {
+    const duck = new MallardDuck()
+    const spy = jest.spyOn(duck, 'swim')
+    duck.swim()
+    expect(duck.swim).toBeCalled()
+    spy.mockReset()
+    spy.mockRestore()
+  })
+  test('Mallard duck should display', () => {
+    const duck = new MallardDuck()
+    const spy = jest.spyOn(duck, 'display')
+    duck.display()
+    expect(duck.display).toBeCalled()
+    spy.mockReset()
+    spy.mockRestore()
   })
 })
