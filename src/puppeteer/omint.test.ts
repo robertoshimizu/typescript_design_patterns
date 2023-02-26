34449ee91ca469334e7312540e5a032411f6e882
robertoshimizu <rshimizubr@gmail.com>
2023-02-26 16:58:42 -0300

    test: switch between tabs

--- src/puppeteer/omint.test.ts
+++ src/puppeteer/omint.test.ts
@@ -4,6 +4,7 @@ import puppeteer from 'puppeteer'
 describe('Omint - connect to a opened browser', () => {
   let browser: Browser
   let page: Page
+  let element: ElementHandle<any>
 
   beforeAll(async () => {
     const browserURL = 'http://127.0.0.1:9222'
@@ -22,7 +23,18 @@ describe('Omint - connect to a opened browser', () => {
   test('switch tab between info pessoal and cotacao', async () => {
     page = (await browser.pages())[0]
     try {
-      const info_tab = await page.waitForXPath('//a[@id="BTN_118000728"]')
+      // seleccao COTACAO
+      element = await page.waitForXPath('//a[@id="BTN_118000728"]')
+      await element.click()
+      element = await page.waitForXPath('//button[@id="BTN_118000550"]')
+      expect(element).toBeTruthy()
+    } catch (error) {}
+    try {
+      // seleccao INFO PESSOAL
+      element = await page.waitForXPath('//a[@id="BTN_118000715"]')
+      await element.click()
+      element = await page.waitForXPath('//button[@id="BTN_118000730"]')
+      expect(element).toBeTruthy()
     } catch (error) {}
   })
 })
