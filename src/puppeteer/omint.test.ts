6927dbad75a4d8f31d36475ba8fb459ba857a859
Roberto Shimizu <rshimizubr@gmail.com>
2023-04-24 14:45:15 -0300

    add comment

--- src/puppeteer/omint.test.ts
+++ src/puppeteer/omint.test.ts
@@ -1,6 +1,12 @@
 import { Browser, ElementHandle, Page } from 'puppeteer'
 import puppeteer from 'puppeteer'
 
+// Open a terminal and run the following command:
+// chrome --remote-debugging-port=9222
+
+// it should open a new browser with a message like this
+// DevTools listening on ws://127.0.0.1:9222/devtools/browser/65b9a7c4-dbfb-483c-aafd-c7ecbf68e244
+
 describe('Omint - connect to a opened browser', () => {
   let browser: Browser
   let page: Page
@@ -20,21 +26,31 @@ describe('Omint - connect to a opened browser', () => {
     page = (await browser.pages())[0]
     expect(page.url()).toContain('omintseguros.com.br')
   })
-  test('switch tab between info pessoal and cotacao', async () => {
+  // test('switch tab between info pessoal and cotacao', async () => {
+  //   page = (await browser.pages())[0]
+  //   try {
+  //     // seleccao COTACAO
+  //     element = await page.waitForXPath('//a[@id="BTN_118000728"]')
+  //     await element.click()
+  //     element = await page.waitForXPath('//button[@id="BTN_118000550"]')
+  //     expect(element).toBeTruthy()
+  //   } catch (error) {}
+  //   try {
+  //     // seleccao INFO PESSOAL
+  //     element = await page.waitForXPath('//a[@id="BTN_118000715"]')
+  //     await element.click()
+  //     element = await page.waitForXPath('//button[@id="BTN_118000730"]')
+  //     expect(element).toBeTruthy()
+  //   } catch (error) {}
+  // })
+  test('select and change birthdate', async () => {
     page = (await browser.pages())[0]
-    try {
-      // seleccao COTACAO
-      element = await page.waitForXPath('//a[@id="BTN_118000728"]')
-      await element.click()
-      element = await page.waitForXPath('//button[@id="BTN_118000550"]')
-      expect(element).toBeTruthy()
-    } catch (error) {}
-    try {
-      // seleccao INFO PESSOAL
-      element = await page.waitForXPath('//a[@id="BTN_118000715"]')
-      await element.click()
-      element = await page.waitForXPath('//button[@id="BTN_118000730"]')
-      expect(element).toBeTruthy()
-    } catch (error) {}
+
+    // selecionar data de nascimento
+    const birthdate: ElementHandle<any> = await page.waitForXPath(
+      '//input[@id="dt_nasc"]'
+    )
+    expect(birthdate.).toBeTruthy()
+    await birthdate.type('12051990')
   })
 })
