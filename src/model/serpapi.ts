a80f336db02af9487c4808e42e5547ddf89e1a69
Roberto Shimizu <rshimizubr@gmail.com>
2024-01-14 12:58:21 -0300

    interface pageContent

--- src/model/serpapi.ts
+++ src/model/serpapi.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 import { SerpAPILoader } from 'langchain/document_loaders/web/serpapi'
-import { type Document } from '../respository/local_document_store'
+import { type Documento } from '../respository/local_document_store'
 
 interface SerpApiPayload {
   engine: string
@@ -33,7 +33,7 @@ export class SerpApi {
     this.api_key = SERPAPI_API_KEY
   }
 
-  async searchLink (query: string): Promise<Document[]> {
+  async searchLink (query: string): Promise<Documento[]> {
     const source = 'site:scielo.br/ OR site:ncbi.nlm.nih.gov/pmc/ OR site:cochranelibrary.com/ OR site:drugs.com/ OR site:medscape.com/ OR site:ncbi.nlm.nih.gov/books/ OR site:merckmanuals.com/professional/ OR site:nice.org.uk/guidance/ OR site:who.int/publications/ OR site:cdc.gov'
     const payload: SerpApiPayload = {
       engine: 'google',
@@ -45,7 +45,7 @@ export class SerpApi {
     try {
       const loader = new SerpAPILoader(payload)
       const docs = await loader.load()
-      return docs.map(doc => ({
+      const allDocs = docs.map(doc => ({
         ...doc,
         metadata: {
           source: '',
@@ -53,6 +53,13 @@ export class SerpApi {
           ...doc.metadata
         }
       }))
+      if (allDocs.length === 0) {
+        throw new Error('No documents found.')
+      }
+      if (allDocs.length < 3) {
+        return allDocs
+      }
+      return allDocs.slice(0, 3)
     } catch (error) {
       console.log('Error retrieving SerpAPILoader:', error)
       return []
