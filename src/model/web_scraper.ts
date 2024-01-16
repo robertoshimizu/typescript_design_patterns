e8911f43e5b7e25504a8722d329d8acbcaa7ee08
Roberto Shimizu <rshimizubr@gmail.com>
2024-01-16 15:17:39 -0300

    wip

--- src/model/web_scraper.ts
+++ src/model/web_scraper.ts
@@ -1,5 +1,14 @@
+/* eslint-disable no-case-declarations */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import cheerio from 'cheerio'
+import { ChatAnthropic } from '@langchain/anthropic'
+import { config } from 'dotenv'
+import { PromptTemplate } from '@langchain/core/prompts'
 
+// Load environment variables from .env file
+config()
+
+const api_key = process.env.ANTHROPIC_API_KEY
 interface IScraper {
   scrape: (url: string) => Promise<string>
 }
@@ -14,6 +23,18 @@ function extractPMCNumber (url: string): string | null {
   return (match != null) ? match[1] : null
 }
 
+function extractFromXML (xml: string): any {
+  const $ = cheerio.load(xml, { xmlMode: true })
+
+  // Extracting the entire <book-part book-part-type="chapter"> and its children
+  const chapter = $('book-part[book-part-type="chapter"]').html()
+
+  // Extracting the <body> tag and its children within the chapter
+  const body = $('book-part[book-part-type="chapter"]').find('body').html()
+
+  return body
+}
+
 class StatsPearlScraper implements IScraper {
   async scrape (url: string): Promise<string> {
     // Specific scraping logic for books
@@ -22,9 +43,28 @@ class StatsPearlScraper implements IScraper {
     try {
       const response = await fetch(api)
       if (response.ok) {
-        console.log(response.type)
         const scrape = await response.text()
-        return scrape
+        const body = extractFromXML(scrape)
+        const contents: Array<{ tag: string, content: string }> = []
+        body.find('title, p').each((index, element) => {
+          const tag = $(element).get(0).tagName
+          const content = $(element).text()
+          contents.push({ tag, content })
+        })
+        const summaryTemplate = PromptTemplate.fromTemplate(
+          '<<< {text} >>> Using the above text, summarize the text. Include all factual information, numbers, stats etc if available.'
+        )
+
+        const model = new ChatAnthropic({
+          modelName: 'claude-2.1',
+          temperature: 0.0,
+          anthropicApiKey: api_key
+        })
+        console.log(contents)
+        const prompt = await summaryTemplate.format({ text: contents })
+        const scrapey = await model.invoke(prompt)
+        console.log(scrapey.content)
+        return scrapey.content
       }
       return ' '
     } catch (error) {
@@ -88,7 +128,7 @@ class OthersScraper implements IScraper {
   }
 }
 
-export async function webScraper (category: string, url: string): Promise<string> {
+export async function webScraper (category: string, url: string): any {
   let scraper: IScraper
 
   switch (category) {
@@ -109,5 +149,7 @@ export async function webScraper (category: string, url: string): Promise<string
       break
   }
 
-  return scraper.scrape(url)
+  const res = await scraper.scrape(url)
+
+  return res
 }
